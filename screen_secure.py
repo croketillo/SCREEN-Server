6a0af6ca9548d8585c17ac2d20df2e4afdc6a2c9
croketillo <jose_carrizosa@hotmail.com>
2023-10-02 07:12:39 +0200

    Implement security token

--- screen_secure.py
+++ screen_secure.py
@@ -1,13 +1,3 @@
-"""
-		SCREEN-SERVER
-		Version 1.0git@github.com:croketillo/screen-server.git
-
-	Create a server http to serve images por advertising screens
-
- 	Author: Croketillo (croketillo@gmail.com)
-	GITHUB: https://github.com/croketillo
-"""
-
 import http.server
 import socketserver
 import threading
@@ -17,18 +7,20 @@ import json
 
 # Server Configuration
 PORT = 8800
-IMAGE_FOLDER = "images"  # Folder where images are stored
-CONFIG_FILE = "config.json"  # Configuration file
+IMAGE_FOLDER = "images"  # Carpeta donde se almacenan las imágenes
+CONFIG_FILE = "config.json"  # Archivo de configuración
 
-# Variables to store configuration and images
+# Secret key for generating and validating tokens
+SECRET_KEY = "tu_clave_secreta"  # Reemplaza con tu propia clave secreta
+
+# Variables para almacenar la configuración y las imágenes
 config = {}
 image_list = []
 current_image_data = None
 current_image_index = 0
 
-
+# Función para cargar la configuración desde el archivo JSON
 def load_config():
-    # Function to load configuration from JSON file
     global config, image_list
     try:
         with open(CONFIG_FILE, "r") as config_file:
@@ -36,14 +28,13 @@ def load_config():
             config = config_data.get("config", {})
             image_list = config_data.get("images", [])
     except FileNotFoundError:
-        print(f"The configuration file {CONFIG_FILE} is not found.")
+        print(f"El archivo de configuración {CONFIG_FILE} no se encuentra.")
 
-
+# Función para cargar la siguiente imagen y su tiempo de visualización
 def load_next_image():
-    # Function to load the next image and its display duration
     global current_image_data, current_image_index
     if not image_list:
-        return None  # Return None if there are no images in the configuration
+        return None  # Devolver None si no hay imágenes en la configuración
 
     current_image_info = image_list[current_image_index]
     image_name = current_image_info.get("name", "")
@@ -54,28 +45,37 @@ def load_next_image():
         with open(image_path, "rb") as f:
             current_image_data = f.read()
     else:
-        print(f"The image {image_name} is not found in the folder {IMAGE_FOLDER}")
+        print(f"La imagen {image_name} no se encuentra en la carpeta {IMAGE_FOLDER}")
 
     current_image_index = (current_image_index + 1) % len(image_list)
     return duration
 
-
+# Función para servir la imagen actual
 def serve_current_image():
-    # Function to serve the current image
     if current_image_data:
         return current_image_data
 
-
+# Función para cambiar la imagen actual periódicamente
 def change_image_periodically():
-    # Function to change the current image periodically
     while True:
         duration = load_next_image()
         if duration is not None:
             time.sleep(duration)
 
+# Función para el servidor HTTP
 class ImageHandler(http.server.BaseHTTPRequestHandler):
-    # HTTP Server Function
     def do_GET(self):
+        # Obtener el token del encabezado de autorización
+        token = self.headers.get("Authorization")
+
+        # Verificar si el token es válido
+        if token != f"Bearer {SECRET_KEY}":
+            self.send_response(401)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Unauthorized")
+            return
+
         if self.path == "/get_image":
             self.send_response(200)
             self.send_header("Content-type", "image/jpeg")
@@ -93,20 +93,12 @@ class ImageHandler(http.server.BaseHTTPRequestHandler):
 if __name__ == "__main__":
     load_config()
 
-    # Create a thread to change the current image periodically
+    # Crear un subproceso para cambiar la imagen actual periódicamente
     image_change_timer_thread = threading.Thread(target=change_image_periodically)
     image_change_timer_thread.daemon = True
     image_change_timer_thread.start()
 
-    # Start the server
+    # Iniciar el servidor
     with socketserver.TCPServer(("", PORT), ImageHandler) as httpd:
-        print("""
-┏━┓┏━╸┏━┓┏━╸┏━╸┏┓╻ ┏━┓┏━╸┏━┓╻ ╻┏━╸┏━┓
-┗━┓┃  ┣┳┛┣╸ ┣╸ ┃┗┫ ┗━┓┣╸ ┣┳┛┃┏┛┣╸ ┣┳┛
-┗━┛┗━╸╹┗╸┗━╸┗━╸╹ ╹╹┗━┛┗━╸╹┗╸┗┛ ┗━╸╹┗╸
-
-Version 1.0
-
-        """)
-        print(f"Server RUNNING at (http://0.0.0.0/get_image) in {PORT}:")
+        print(f"Servidor activo en el puerto {PORT}")
         httpd.serve_forever()
